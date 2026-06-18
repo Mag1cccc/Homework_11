4f4df2c226cbcb221ead70c262ce590c7166bcc8
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2026-06-19 00:56:42 +0400

    Add XML splitting functionality (#3)

--- Homework_Files_11/Program.cs
+++ Homework_Files_11/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Homework_Files_11.Task1;
 using Homework_Files_11.Task2;
+using Homework_Files_11.Task3;
 
 namespace Homework_Files_11
 {
@@ -11,8 +12,11 @@ namespace Homework_Files_11
 
             //lineReader.ProcessFile();
 
-            MultiplicationWriter multiplicationWriter = new MultiplicationWriter();
-            multiplicationWriter.CreateTableFile();
+            //MultiplicationWriter multiplicationWriter = new MultiplicationWriter();
+            //multiplicationWriter.CreateTableFile();
+
+            XmlSplitter xmlSplitter = new XmlSplitter();
+            xmlSplitter.SplitAndCreateXML();
         }
     }
 }
